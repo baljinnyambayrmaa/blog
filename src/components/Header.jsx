66588e5e3062415fca1114e91a5284ddf368df66
sorivt <baljinnyambayrmaa@gmail.com>
2024-01-17 11:14:27 +0800

    restored

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Search } from "./SearchBar";
 import { FiMenu } from "react-icons/fi";
+import Link from "next/link";
 
 
 export const Header = () => {
@@ -8,9 +9,9 @@ export const Header = () => {
     <div className="flex md:text-center justify-between w-[390px] md:mx-auto md:w-[1130px] items-center p-[20px] md:py-8 workSansText">
       <Image width={158} height={36} src="/Logo.png" className="" />
       <div className="md:flex hidden justify-center gap-10">
-        <h1>Home</h1>
-        <h1>Blog</h1>
-        <h1>Contact</h1>
+        <Link href={"/"}>Home</Link>
+        <Link href={"blog"}>Blog</Link>
+        <Link href={"contact"}>Contact</Link>
       </div>
         <Search />
         <FiMenu size={32} className="md:hidden"/>
---
+++ src/pages/blog/index.js
@@ -0,0 +1,13 @@
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { BlogData } from "@/components/BlogData";
+
+export default function Blog() {
+    return (
+        <div>
+            <Header/>
+            <BlogData/>
+            <Footer/>
+        </div>
+    )
+}
